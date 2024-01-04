c07a32f797e6776b6164c6e7cee68916093762ab
Anshika Singh <88630031+anshikasinghhacker@users.noreply.github.com>
2024-01-05 00:50:59 +0530

    first commit

---
+++ datatype.js
@@ -0,0 +1,16 @@
+//diffrent types of data type in js are
+
+// 1. String
+//2. int
+//3. bool
+//4. null
+//5. bigint
+//6. null
+//7. undefined
+//8. symbol
+
+let name = "Anshika Singh"
+
+console.log(typeof null) // type null ka object hota hai
+console.log(typeof undefined) //type undefine ka undefine hota h
+console.log(typeof name)
---
+++ variables.js
@@ -0,0 +1,25 @@
+console.log("Hello Shubham") //this is how we print in js
+
+// There are three type of variables in js
+//1. var
+//2. let
+//3. const
+
+const password = 10
+console.log(password)
+//password = 20 // not allowed
+var name = "Anshika"
+const uid1 = 8162
+let branch = "cse"
+let block = "Z"
+
+name = "Ansh"
+uid = 8169
+branch = "cfp"
+block = "P"
+//NOTE : WE CAN ALSO DECLARE A VARIBLE DIRECTLY LOOK BELOW
+// NOTE: NOT USE VAR BCOZ THERE IS AN LIMITATION WITH IT -- SCOPE ISSUE
+// note : if we cannot assign a value to the var then its means it is undefined
+let youracc
+name1 = "minnie"
+console.table([name,uid,branch,block,name, name1,youracc])
