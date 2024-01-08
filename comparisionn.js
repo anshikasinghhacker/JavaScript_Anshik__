71602e92d85e71ed1ef770b820266f220d041309
Anshika Singh <88630031+anshikasinghhacker@users.noreply.github.com>
2024-01-08 19:03:49 +0530

    comparision in js

---
+++ comparisionn.js
@@ -0,0 +1,24 @@
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2<=10)
+console.log(2==1)
+console.log(2==2)
+console.log(2!=2)
+
+console.log("2" > 2)
+
+
+console.log(null>0)
+console.log(null>=0)
+console.log(null==0)
+//note comparision convert null to no. treating it as 0
+//that's why null>=0 is true & null>0 is false
+
+
+console.log(undefined>0)
+console.log(undefined>=0)
+console.log(undefined==0)
+
+//=== strict check
+console.log("2"=== 2)
