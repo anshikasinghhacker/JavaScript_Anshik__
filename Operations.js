ac3a7c24e2f659e17273aa68ac6d404efe840be7
Anshika Singh <88630031+anshikasinghhacker@users.noreply.github.com>
2024-01-09 12:53:15 +0530

    operation

---
+++ Operations.js
@@ -0,0 +1,30 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+
+let str1 = "Anshika"
+let str2 = " Singh"
+console.log(str1+str2)
+
+console.log("1"+2)   //12
+console.log(1 + "2") //12
+console.log("1"+2+2) //122
+console.log(1+2+"2") //32
+
+//console.log(true+) // error
+console.log(+true) //1
+console.log(+"") //0
+
+let gameCounter1 = 100
+++gameCounter1  //preincement
+console.log(gameCounter1)
+
+let gameCounter2 = 100
+gameCounter2++ //post increment
+console.log(gameCounter2)
