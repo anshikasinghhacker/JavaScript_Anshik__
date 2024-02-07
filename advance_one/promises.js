9ac0d03ed2fcdef5dd85b7494a4cca36de63af62
Anshika Singh <88630031+anshikasinghhacker@users.noreply.github.com>
2024-02-08 00:43:51 +0530

    js

--- advance_one/promises.js
+++ advance_one/promises.js
@@ -75,3 +75,24 @@ async function consumepromiseFive(){
 }
 }
 consumepromiseFive()
+
+// async function getAllUsers() {
+//  try{ 
+    //   const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     // console.log(response);
+//  const data = await response.json()
+//     console.log(data);
+//  }catch (error){
+//     console.log("E:",error);
+//  }
+// }
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error) => console.log(error));
