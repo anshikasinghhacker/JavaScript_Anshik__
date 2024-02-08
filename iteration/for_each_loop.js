792cd93f90df57c6d6f37ad94509d3125d60ef24
Anshika Singh <88630031+anshikasinghhacker@users.noreply.github.com>
2024-02-08 22:35:44 +0530

    js

--- iteration/for_each_loop.js
+++ iteration/for_each_loop.js
@@ -1,38 +1,38 @@
-// const info = ["Anshika", "Anjali", "Harsh"]
-// info.forEach(function (key) {
-//     console.log(key);
-// });
+const info = ["Anshika", "Anjali", "Harsh"]
+info.forEach(function (key) {
+    console.log(key);
+});
 
 //apply on arrow fun
-// const info1 = ["Anshika", "Anjali", "Harsh"]
-// info1.forEach((key) => {
-//     console.log(key);
-// });
+const info1 = ["Anshika", "Anjali", "Harsh"]
+info1.forEach((key) => {
+    console.log(key);
+});
 
 
-// const programming = ["js", "cpp", "java","py"]
-// programming.forEach( (item , index, arr) => {
-//     console.log(item , index, arr);
-// })
+const programming = ["js", "cpp", "java","py"]
+programming.forEach( (item , index, arr) => {
+    console.log(item , index, arr);
+})
 
-// const myArr = [
-//     {
-//         langName : "Javascript",
-//         langfilename : "js"
-//     },
-//     {
-//         langName : "Java",
-//         langfilename : "java"
-//     },
-//     {
-//         langName : "python",
-//         langfilename : "py"
-//     }
-// ]
+const myArr = [
+    {
+        langName : "Javascript",
+        langfilename : "js"
+    },
+    {
+        langName : "Java",
+        langfilename : "java"
+    },
+    {
+        langName : "python",
+        langfilename : "py"
+    }
+]
 
-// myArr.forEach((item) =>{
-//     console.log(item.langName);
-// })
+myArr.forEach((item) =>{
+    console.log(item.langName);
+})
 
 //note : for each does not return any value
 const myfamily = ["me", "father","mother","brother","sister"]
