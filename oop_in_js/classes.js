ab926dc8589b8957663eb559b8903e6643311297
Anshika Singh <88630031+anshikasinghhacker@users.noreply.github.com>
2024-02-19 21:59:05 +0530

    js

--- oop_in_js/classes.js
+++ oop_in_js/classes.js
@@ -24,3 +24,15 @@ function User(username, email, password){
     this.email = email;
     this.password = password
 }
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+const tea = new User("tea","tea@gmail.com","123")
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
