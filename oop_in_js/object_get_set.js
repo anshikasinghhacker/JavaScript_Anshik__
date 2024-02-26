1946d23f7e6219212d654bc600f27c0f563e4d2b
Anshika Singh <88630031+anshikasinghhacker@users.noreply.github.com>
2024-02-26 17:34:38 +0530

    js

---
+++ oop_in_js/object_get_set.js
@@ -0,0 +1,13 @@
+const User = {
+    _email : 'ansh@a.com',
+    _password: "abc",
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+    set email(value){
+        this._email = value
+    }
+}
+const tea = Object.create(User)
+console.log(tea.email);
